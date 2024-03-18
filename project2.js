5f747707787f1d38da2678547c12d68c2cc0ef23
jbossgotskillz <160427209+jbossgotskillz@users.noreply.github.com>
2024-03-18 00:10:24 -0500

    Add files via upload
    Added CSS and JS files

---
+++ project2.js
@@ -0,0 +1,22 @@
+$(document).ready( () => {
+    $('.needs-validation').submit( event => {
+        if (! event.target.checkValidity() ) {
+            event.preventDefault();
+            event.stopPropagation();          
+        }
+        $(event.target).addClass('was-validated');
+    });
+});
+
+window.onload = () => {
+    const $forms = document.querySelectorAll('.needs-validation');
+    for ( let form of $forms ) {
+        form.addEventListener( 'submit', event => {
+            if (! event.target.checkValidity() ) {
+                event.preventDefault();
+                event.stopPropagation();          
+            }
+            event.target.classList.add('was-validated');
+        });
+    }
+}
