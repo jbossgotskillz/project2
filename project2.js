5bbf7c7076efddda630fa69b25f5f338dc3cbc92
jbossgotskillz <160427209+jbossgotskillz@users.noreply.github.com>
2024-03-18 00:58:54 -0500

    Update project2.js
    Added search engine

--- project2.js
+++ project2.js
@@ -20,3 +20,57 @@ window.onload = () => {
         });
     }
 }
+
+
+//API Search Engine
+
+
+
+
+function getUserInput() { 
+    var inputValue = document.querySelector(".js-userinput").value; 
+    return inputValue; 
+}
+  
+
+
+document.querySelector(".js-go").addEventListener("click", function () { 
+    var inputValue = document.querySelector(".js-userinput").value; 
+    var userInput = getUserInput(); 
+    searchGiphy(userInput); 
+});
+
+document.querySelector(".js-userinput") 
+.addEventListener("keyup", function (e) {
+    if (e.which === 13) { 
+
+        var userInput = getUserInput(); 
+	    searchGiphy(userInput); 
+    } 
+}); 
+
+function searchGiphy(searchQuery) { 
+var url = "https://api.giphy.com/v1/gifs/search?api_key=AXjWnGqWrRzgQq55DaBNv9M72V3cPTX8&q="
++ searchQuery; 
+	
+var GiphyAJAXCall = new XMLHttpRequest(); 
+GiphyAJAXCall.open("GET", url); 
+GiphyAJAXCall.send(); 
+
+GiphyAJAXCall.addEventListener("load", function (data) { 
+	var actualData = data.target.response; 
+	pushToDOM(actualData); 
+	console.log(actualData); 
+	
+}); 
+} 
+function pushToDOM(response) { 
+    response = JSON.parse(response); 
+    var images = response.data; 
+    var container = document.querySelector(".js-container"); 
+    container.innerHTML = ""; 
+    images.forEach(function (image) { 
+        var src = image.images.fixed_height.url; 
+        container.innerHTML += "<img src='"+ src + "' class='container-image' />"; 
+}); 
+}
